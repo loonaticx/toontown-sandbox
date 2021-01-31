ca8e4d6a3e84802af580ab63d16952273582a2a8
loonaticx <l.oony@aol.com>
2021-01-31 02:26:54 -0600

    Match Minnie: Impossible Difficulty
    Hehehehe

--- toontown/minigame/PatternGameGlobals.py
+++ toontown/minigame/PatternGameGlobals.py
@@ -1,7 +1,7 @@
 import MinigameGlobals
 INITIAL_ROUND_LENGTH = 2
 ROUND_LENGTH_INCREMENT = 2
-NUM_ROUNDS = 4
+NUM_ROUNDS = 99
 TOONTOWN_WORK = 1
 InputTime = 10
 ClientsReadyTimeout = 5 + MinigameGlobals.latencyTolerance
