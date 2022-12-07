3d53af138fe7a797750b052b9c59b50c13236b0c
Erica M. ("Loonatic") <l.oony@aol.com>
2022-12-07 01:08:23 -0600

    Factory: code cleanup and documentation

--- modular/toontown/coghq/DistributedFactory.py
+++ modular/toontown/coghq/DistributedFactory.py
@@ -1,19 +1,22 @@
-from direct.fsm import State
+"""
+Simulates a basic DistributedFactory instance; a lot of the entities are not completely generated.
+"""
 
-
+# Init modular interface
 from modular.toontown.toonbase import ModularStart
 from modular.toontown.toonbase.ModularBase import ModularBase
-
 base = ModularBase()
 base.initCR()
 base.generateLocalAvatar()
 
+from direct.fsm import State
 from toontown.coghq.DistributedFactory import DistributedFactory
 
+# Generate our DistributedFactory
 dfact = DistributedFactory(base.cr)
 dfact.doId = 2
 dfact.generate()
-dfact.factoryId = 11500
+dfact.factoryId = 11500  # ID that sellbot factory uses
 dfact.zoneIds = []
 for i in range(1, 999):
     dfact.zoneIds.append(i)
@@ -25,21 +28,25 @@ dfact.levelZone = 1
 base.localAvatar.defaultShard = 0
 dfact.dclass = base.cr.dclassesByName['DistributedFactory']
 dfact.levelAnnounceGenerate()
+
+# Generate Level using factory level spec
 from toontown.coghq.SellbotLegFactorySpec import *
 from otp.level import LevelSpec
 factSpec = LevelSpec.LevelSpec(levelSpec)
 dfact.privGotSpec(factSpec)
+
+# Place down our avatar
 dfact.placeLocalToon()
 
 entityCreator = dfact.createEntityCreator()
 
+# Show all of the zones since we dont wanna deal with zone interests/visibility checks
 for zone in dfact.zoneIds:
     dfact.showZone(zone)
 
+# Makes our avatar visible to us
 base.localAvatar.reparentTo(render.find("**/LegFactory"))
 # base.localAvatar.setPos( 20.7079, 26.1823, 3.72601)
 
-
 base.oobe()
-
 base.run()
